8cee25ce58641027ce0e28413444d4b765cde890
Luis J. Espinoza <luis.espinoza@qompa.io>
2025-03-14 18:47:20 -0400

    feat: terraform's region output

--- outputs.tf
+++ outputs.tf
@@ -25,3 +25,8 @@ output "cluster_ca_certificate" {
   value       = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
   sensitive   = true
 }
+
+output "region" {
+  description = "The region where the GKE cluster is deployed"
+  value       = var.region
+}
